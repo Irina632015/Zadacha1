f0e05468d7aa47628421deab3bec1ffae99c6cb4
Irina632015 <irina.632015@yandex.ru>
2023-01-26 20:41:02 +0400

    Task 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 > Num2)
+
+{
+    Console.WriteLine(Num1 + " > " + Num2);
+}
+else if (Num2 > Num1)
+{
+    Console.WriteLine(Num2 + " > " + Num1);
+}
+else
+{
+    Console.WriteLine(Num2 + " = " + Num1);
+}
